87659038d75481907263ea342830a06f1d205cf8
chenletian-sky <446075873@qq.com>
2021-12-15 13:55:28 +0800

    change dbUrl

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 var app = {
   // "dbUrl":"mongodb://test:test@127.0.0.1:27017/mongoosedb"
   // "dbUrl":"mongodb://localhost:27017/test"
-  "dbUrl":"mongodb://localhost:27017/mongoosedb"
+  // "dbUrl":"mongodb://localhost:27017/mongoosedb",
+  "dbUrl":"mongodb://101.35.15.228:27017/mongoosedb"
 }
 
 module.exports = app
